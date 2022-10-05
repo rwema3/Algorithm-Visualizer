bd081c508aef769605d0258037968fd28e800789
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-05 15:34:22 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -103,6 +103,15 @@ class _ListPageState extends State<ListPage> with TickerProviderStateMixin {
           physics: AlwaysScrollableScrollPhysics(),
           pageSnapping: true,
           scrollDirection: Axis.horizontal,
+          onPageChanged: (int newPage) {
+            setState(() {
+				setState(() {
+					activePage = newPage;
+				});
+            });
+          },
+          children: createChildren(makeCard),
+        ));
 
 
 /* sources>
