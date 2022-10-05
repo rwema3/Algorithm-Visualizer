7b635f17b3b85fd6c11327de50e3231e6ca7cbca
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-05 15:37:40 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -257,7 +257,12 @@ List<Lesson> getLessons() => [
           simulationDetails: 7,
           algorithmTemplate: AlgorithmTemplate.graph,
 		  algorithmType: AlgorithmTypes.pathFinding,
-          
+          content:
+		  "Floyd–Warshall algorithm is an algorithm for finding shortest paths in a weighted graph with positive or negative edge weights (but with no negative cycles).A single execution of the algorithm will find the lengths (summed"
+              " weights) of shortest paths between all pairs of vertices. Although it does not return details of the paths themselves, it is possible to reconstruct the paths with simple modifications to the algorithm. "
+              "Floyd-Warshall's algorithm is used when any of all the nodes can be a source, so you want the shortest distance to reach any destination node from any source node. Floyd-Warshall computes shortest paths from each node to "
+			  "every other node."),
+      Lesson(
           title: "Johnson's algorithm",
           level: "Advanced",
           indicatorValue: 1.0,
