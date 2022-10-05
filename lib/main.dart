a52623d68f4d68221cc5d65e1fa5d366cc9bddd1
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-05 15:32:33 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,3 @@ import 'package:AlgorithmVisualizer/controllers/Controllers.dart';
 import 'package:AlgorithmVisualizer/detailPages/detail_page.dart';
 
 
-/* sources>
-
-  https://brilliant.org/wiki/shortest-path-algorithms/
-
- */
