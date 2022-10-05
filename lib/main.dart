b06af4c82176c1bb5f6bfc6fdb68263c68a9ea27
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-05 15:29:27 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -136,12 +136,6 @@ class _ListPageState extends State<ListPage> with TickerProviderStateMixin {
 				  }
 				  _controllers.pageController.animateToPage(activePage, duration: Duration(seconds: 1), curve: Curves.ease);
 				  //toggleIcon(activePage + _controllers.iconAnimationControllerPositionShift);
-              },
-            );
-          },
-        ),
-      ),
-    );
 
 
 /* sources>
