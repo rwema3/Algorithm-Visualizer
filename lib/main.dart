10b0129c66e465c48130bd35e5817f34748be126
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-05 15:33:20 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:AlgorithmVisualizer/controllers/Controllers.dart';
 import 'package:AlgorithmVisualizer/detailPages/detail_page.dart';
+import 'package:AlgorithmVisualizer/model/lesson.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  final Controllers _controllers = Controllers();
+
 
 
 /* sources>
