c96e0e90251248599a8347d7fafe961d55085cb5
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-05 15:33:09 -0700

    Update simulation_state.dart

--- lib/simulation/simulation_state.dart
+++ lib/simulation/simulation_state.dart
@@ -1,1 +1,11 @@
 
+
+class GameWrapper extends StatefulWidget {
+  final SimulationAlgorithm simulation;
+  final Controllers _controllers;
+
+  GameWrapper(this.simulation, this._controllers);
+
+  @override
+  Game createState() => Game(simulation, _controllers);
+}
