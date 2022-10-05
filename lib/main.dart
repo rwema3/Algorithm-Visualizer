343b2cef6d4375801492d9bf3c7fbcbfe20d1d67
Dominique Rwema Bagirishya <52289151+rwema3@users.noreply.github.com>
2022-10-05 15:36:20 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -209,6 +209,15 @@ List<Lesson> getLessons() => [
           usages: "Single-source shortest path in a graph",
           simulationDetails: 7,
           additionalInformation: 3,
+          algorithmTemplate: AlgorithmTemplate.graph,
+		  algorithmType: AlgorithmTypes.pathFinding,
+          content:
+		  "Dijkstra's algorithm is an algorithm for finding the shortest paths between nodes in a graph, which may represent, for example, road networks. The algorithm exists in many variants; Dijkstra's original variant found the "
+              "shortest path between two nodes, but a more common variant fixes a single node as the 'source' node and finds shortest paths from the source to all other nodes in the graph, producing a shortest-path tree."),
+      Lesson(
+          title: "A-star algorithm",
+          level: "Intermediate",
+          indicatorValue: 0.6,
           
 
 /* sources>
