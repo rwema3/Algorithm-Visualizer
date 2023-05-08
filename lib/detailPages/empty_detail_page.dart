740f6f3e3baa29dd3e6621b4b6f7d6b69177301d
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-08 23:49:45 +0200

    EmptyHomePage

--- lib/detailPages/empty_detail_page.dart
+++ lib/detailPages/empty_detail_page.dart
@@ -1,3 +1,8 @@
+  EmptyHomePage(Lesson lesson, Controllers controllers) : super(lesson, controllers);
+
+  @override
+  Container bottomContent() {
+    return Container(
       width: MediaQuery.of(context).size.width,
       padding: EdgeInsets.all(30.0),
       child: Center(
