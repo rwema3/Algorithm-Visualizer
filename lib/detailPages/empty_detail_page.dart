68191e11772b45d25cdb86da4e128f6a7c99b24d
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-08 23:49:32 +0200

    MediaQuery.

--- lib/detailPages/empty_detail_page.dart
+++ lib/detailPages/empty_detail_page.dart
@@ -1,3 +1,7 @@
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.all(30.0),
+      child: Center(
+        child: Column(
           children: <Widget>[getBottomContentText()],
         ),
       ),
