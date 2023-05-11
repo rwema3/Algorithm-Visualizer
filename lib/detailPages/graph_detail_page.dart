d88c0dc371009d260bf889157e985f9a10623c3b
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-11 23:58:11 +0200

    GraphHomePage

---
+++ lib/detailPages/graph_detail_page.dart
@@ -0,0 +1,11 @@
+part of 'detail_page.dart';
+
+class GraphHomePage extends HomePage {
+  GraphHomePage(Lesson lesson, Controllers controllers) : super(lesson, controllers);
+
+  Container getSimulationStepSwitch(BuildContext context) {
+	  return Container(
+		  padding: EdgeInsets.symmetric(vertical: 16.0),
+		  width: MediaQuery
+			  .of(context)
+
